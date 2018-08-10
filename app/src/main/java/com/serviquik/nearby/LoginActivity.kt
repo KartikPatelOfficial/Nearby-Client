00ab8a5737b8901fc1c961110033534178238cae
Kartik Patel <patelkartik1910@gmail.com>
2018-08-11 00:32:55 +0530

    Add: Login Design
    Add: login page design
    
    ChangeLog: #2

--- app/src/main/java/com/serviquik/nearby/LoginActivity.kt
+++ app/src/main/java/com/serviquik/nearby/LoginActivity.kt
@@ -2,11 +2,17 @@ package com.serviquik.nearby
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val ft = supportFragmentManager!!.beginTransaction()
+        ft.replace(R.id.LoginFramLayout, SignInFragment(), "RootFragment")
+        ft.commit()
+
     }
 }
---
+++ app/src/main/java/com/serviquik/nearby/SignInFragment.kt
@@ -0,0 +1,19 @@
+package com.serviquik.nearby
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignInFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_sign_in, container, false)
+
+        return view
+    }
+
+}
