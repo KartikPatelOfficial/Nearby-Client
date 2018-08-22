e7cf05b9c8e0449a0f9fe29274c856a4e8decd66
Kartik Patel <patelkartik1910@gmail.com>
2018-08-22 18:14:27 +0530

    Add: PDF ChangeLog #15

--- app/src/main/java/com/serviquik/nearby/bill/Bill.kt
+++ app/src/main/java/com/serviquik/nearby/bill/Bill.kt
@@ -1,7 +1,6 @@
 package com.serviquik.nearby.bill
 
-class Bill(
-        var productName: String,
-        var productPrice: Long,
-        var quantity: Int?
-)
+class Bill(var productName: String?, var productPrice: Long?, var quantity: Int?)
+
+
+
--- app/src/main/java/com/serviquik/nearby/bill/BillFragment.kt
+++ app/src/main/java/com/serviquik/nearby/bill/BillFragment.kt
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -15,6 +16,23 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.serviquik.nearby.R
 import com.serviquik.nearby.customer.ManageCustomerFragment
 import com.serviquik.nearby.manageProduct.Product
+import android.widget.Toast
+import android.content.Intent
+import android.os.Environment
+import android.util.Log
+import com.google.firebase.Timestamp
+import com.itextpdf.text.*
+import com.itextpdf.text.pdf.PdfWriter
+import java.io.FileOutputStream
+import java.util.*
+import kotlin.collections.HashMap
+import com.itextpdf.text.pdf.BaseFont
+import com.itextpdf.text.pdf.draw.LineSeparator
+import com.itextpdf.text.Paragraph
+import java.io.File
+import java.io.FileNotFoundException
+import java.text.SimpleDateFormat
+
 
 @SuppressLint("StaticFieldLeak")
 class BillFragment : Fragment() {
@@ -25,7 +43,7 @@ class BillFragment : Fragment() {
 
 
     companion object {
-        var total:Long = 0
+        var total: Long = 0
         var count = 0
         val currentProducts = ArrayList<Bill>()
         var adapter: BillAdapter? = null
@@ -53,6 +71,10 @@ class BillFragment : Fragment() {
         totalText = view.findViewById(R.id.billTotalTV)
         totalText.text = text
 
+        view.findViewById<Button>(R.id.billSendBillBtn).setOnClickListener {
+            sendToDatabase()
+        }
+
         db.collection("Products").whereEqualTo("VendorID", auth.uid).get().addOnCompleteListener {
             if (it.isSuccessful) {
                 for (document in it.result) {
@@ -67,6 +89,77 @@ class BillFragment : Fragment() {
         return view
     }
 
+    private fun sendToDatabase() {
+        val data = HashMap<String, Any>()
+        data["Price"] = total
+        data["Time"] = Timestamp.now()
 
+        val orderRef = db.collection("Vendors").document(auth.uid!!).collection("OrderList").document()
+
+        orderRef.set(data).addOnSuccessListener { _ ->
+            for (bill in currentProducts) {
+                val items = HashMap<String, Any>()
+                items["Name"] = bill.productName!!
+                items["Quantity"] = bill.quantity!!
+                items["Price"] = bill.productPrice!!
+                orderRef.collection("Items").document().set(items)
+            }
+            sendMail()
+        }
+    }
+
+    private fun sendMail() {
+        //Todo add shop name
+        val shopName = "Temp"
+        var invocation = ""
+
+        for (bill in currentProducts) {
+            invocation += "${bill.productName}\t\t\t\t\t ${bill.quantity}\t\t\t\t\t ${bill.productPrice}\n"
+        }
+
+        val textMail = "Your order from $shopName \n\n $invocation \n\n Grand total : $total"
+
+        val i = Intent(Intent.ACTION_SEND)
+        i.type = "message/rfc822"
+        i.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        i.putExtra(Intent.EXTRA_SUBJECT, "Receipt")
+        i.putExtra(Intent.EXTRA_TEXT, textMail)
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."))
+            createPdf()
+        } catch (ex: android.content.ActivityNotFoundException) {
+            Toast.makeText(context, "There are no email clients installed.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun createPdf() {
+
+
+        val pdfFolder = File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS), "pdfdemo")
+        if (!pdfFolder.exists()) {
+            pdfFolder.mkdir()
+        }
+
+        val date = Date()
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(date)
+
+        val myFile = File("$pdfFolder$timeStamp.pdf")
+
+        val output = FileOutputStream(myFile)
+
+        val document = Document()
+
+        PdfWriter.getInstance(document, output)
+
+        document.open()
+
+        document.add(Paragraph("Hey there"))
+        document.add(Paragraph("Base camp"))
+
+        document.close()
+
+    }
 
 }
